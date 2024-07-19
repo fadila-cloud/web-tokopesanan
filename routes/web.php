df3c7f528e60d85048fd9551fb7ceb5c1f8d5e03
Ilma Fadilah <ilmafadilah225@gmail.com>
2024-07-19 16:31:05 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PesananController; 
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+//menambah alamat ke pesanans
+Route::resource('pesanans', PesananController::class); 
